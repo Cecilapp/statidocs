a11c3ff6c1ef9d49620ac01d54d172e17e3c43b6
Arnaud Ligny <arnaud@ligny.org>
2023-05-17 16:48:32 +0200

    Push theme

---
+++ themes/docs/tailwind.preset.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    './layouts/**/*.html.twig',
+    './themes/**/layouts/**/*.html.twig',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue, // https://tailwindcss.com/docs/customizing-colors
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')({
+      className: 'markdown',
+    }),
+  ],
+}
